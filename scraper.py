fe49a397b8838f26f138d91d1ac5701f03c3a6eb
ThatEvilChickenNextDoor <kwzeyunwang@gmail.com>
2019-11-26 12:46:53 -0500

    Updated scraper.py
    Removed section and instructor fields. Added additional scrape toggle to scrape pre- and coreqs for each course.

--- scraper.py
+++ scraper.py
@@ -2,23 +2,32 @@ from bs4 import BeautifulSoup
 import sqlite3
 import requests
 
-LOCAL = False
+import prereqScraper as ps
 
-con = sqlite3.connect(':memory:')
+LOCAL = False
+SCRAPE = True
+
+if SCRAPE:
+    con = sqlite3.connect('store.db')
+else:
+    con = sqlite3.connect(':memory:')
+    
 con.execute('''DROP TABLE IF EXISTS courses;''')
 con.execute('''CREATE TABLE courses (
     crn INTEGER PRIMARY KEY,
-    course TEXT,
-    section INTEGER,
+    course TEXT UNIQUE,
     attr TEXT,
     title TEXT,
-    instr TEXT
+    prereqs TEXT,
+    coreqs TEXT,
+    require TEXT
     );''')
+
 if LOCAL:
     with open('csciCoursePage.html') as page:
         soup = BeautifulSoup(page, 'lxml')
 else:
-    URL = 'https://courselist.wm.edu/courselist/courseinfo/searchresults?term_code=202020&term_subj=CSCI&attr=0&attr2=0&levl=UG&status=0&ptrm=0&search=Search'
+    URL = 'https://courselist.wm.edu/courselist/courseinfo/searchresults?term_code=202020&term_subj=MATH&attr=0&attr2=0&levl=UG&status=0&ptrm=0&search=Search'
     r = requests.get(URL)
     soup = BeautifulSoup(r.text, 'html5lib')
 table = soup.table
@@ -35,17 +44,23 @@ for row in table_rows:
 
 courses = [[i.text.strip() for i in row.find_all('td')] for row in table_rows][1:]
 
-dat = [[row[0], row[1][:-3], row[1][-2:]] + row[2:5] for row in courses]
-
-#for course in dat:
-#    print(course)
+dat = [[row[0], row[1][:-3]] + row[2:4] for row in courses]
 
 with con:
-    con.executemany('''INSERT INTO courses (crn, course, section, attr, title, instr)
-        VALUES (?, ?, ?, ?, ?, ?);''', dat)
-        
-for course in con.execute('''SELECT DISTINCT course FROM courses ORDER BY course;'''):
-    cur = con.execute('''SELECT crn FROM courses WHERE course=? ORDER BY section;''', course)
-    print(cur.fetchone()[0], course[0])
+    con.executemany('''INSERT OR IGNORE INTO courses (crn, course, attr, title)
+        VALUES (?, ?, ?, ?);''', dat)
+
+if SCRAPE:    
+    cur = con.execute('''SELECT crn FROM courses ORDER BY course;''')
+    q = cur.fetchall()
+    reqs = [crn for tup in q for crn in tup] 
+    
+    res = ps.reqStack(reqs)
+
+    with con:
+        con.executemany('''UPDATE courses SET prereqs=?, coreqs=?, require=? WHERE crn=?;''', res)
+
+for row in con.execute('''SELECT * FROM courses;'''):
+    print(row)
 
 con.close()
